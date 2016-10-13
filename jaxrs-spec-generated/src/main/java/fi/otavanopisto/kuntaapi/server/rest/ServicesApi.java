eda8917b215037d8fc48128f5a895af9bae489d1
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-13 14:55:19 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/otavanopisto/kuntaapi/server/rest/ServicesApi.java
+++ jaxrs-spec-generated/src/main/java/fi/otavanopisto/kuntaapi/server/rest/ServicesApi.java
@@ -23,7 +23,7 @@ import java.util.List;
 @Api(description = "the services API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2016-10-13T14:53:46.079+03:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2016-10-13T14:55:13.135+03:00")
 
 public abstract class ServicesApi extends AbstractApi {
 
