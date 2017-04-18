73fa624a97a63a9a2f10a6fb705f02287f9d42b6
Antti Leppä <antti.leppa@metatavu.fi>
2017-04-18 12:28:17 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/PhoneServiceChannelsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/PhoneServiceChannelsApi.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Api(description = "the phoneServiceChannels API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-04-18T07:00:16.925+03:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-04-18T12:28:08.008+03:00")
 
 public abstract class PhoneServiceChannelsApi extends AbstractApi {
 
