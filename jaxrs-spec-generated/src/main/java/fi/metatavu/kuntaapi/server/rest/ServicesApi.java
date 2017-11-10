3f48332a3d6e8e04dd3dcfe844538dde25d4997b
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-11-10 15:50:02 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/ServicesApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/ServicesApi.java
@@ -25,7 +25,7 @@ import java.util.List;
 @Api(description = "the services API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-11-10T15:46:06.618+02:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-11-10T15:49:52.830+02:00")
 
 public abstract class ServicesApi extends AbstractApi {
 
