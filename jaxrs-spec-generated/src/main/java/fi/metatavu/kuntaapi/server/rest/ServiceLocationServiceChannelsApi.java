94c0ce63c59eb42e57a3a4eed97fc3f4bd7fc0bd
Antti Leppä <antti.leppa@metatavu.fi>
2017-11-16 07:38:09 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/ServiceLocationServiceChannelsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/ServiceLocationServiceChannelsApi.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Api(description = "the serviceLocationServiceChannels API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-11-16T07:30:32.602+02:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-11-16T07:38:00.212+02:00")
 
 public abstract class ServiceLocationServiceChannelsApi extends AbstractApi {
 
