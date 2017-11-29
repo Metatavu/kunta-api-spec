def1a90fe9b30288c5911c08e109ab1e1c6dc262
Antti Leppä <antti.leppa@metatavu.fi>
2017-11-29 05:48:52 +0200

    php version bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   
-  var PHP_CLIENT_VERSION = '1.0.0';
+  var PHP_CLIENT_VERSION = '1.0.6';
   
   grunt.initConfig({
     'clean': {
