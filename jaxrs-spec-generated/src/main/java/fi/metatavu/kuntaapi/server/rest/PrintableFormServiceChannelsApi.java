861a5710386d2ef273c77bd3c55b1deb58050365
Antti Leppä <antti.leppa@metatavu.fi>
2019-03-11 07:44:23 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/PrintableFormServiceChannelsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/kuntaapi/server/rest/PrintableFormServiceChannelsApi.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Api(description = "the printableFormServiceChannels API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2019-03-11T07:39:03.762+02:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2019-03-11T07:44:17.772+02:00")
 
 public abstract class PrintableFormServiceChannelsApi extends AbstractApi {
 
